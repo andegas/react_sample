9475f36d3786c5b274e8dcd167e9322126ded85f
Hayk.Grigoryan <hayk.grigoryan@helpsystems.com>
2021-10-29 12:35:34 +0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './app.css';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                React app
+            </div>
+        );
+    }
+}
+
+
+export default App;
